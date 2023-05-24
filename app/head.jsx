90a6f1d58c664d9fcdddda7ceda2a06ce712f972
Farid <fareedibrahim0003@gmail.com>
2023-05-24 21:13:23 +0100

    html page deleted

--- app/head.jsx
+++ app/head.jsx
@@ -7,7 +7,6 @@ function head() {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Capsul Sign-in</title>
     <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="styles.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
